268cca00943f20d0cbdd8cba0b135b9841d7efa5
Sang Near <nguyenlamsang2004@gmail.com>
2024-08-24 21:57:11 +0700

    fix to deloy

--- app/(root)/[name]/page.tsx
+++ app/(root)/[name]/page.tsx
@@ -1,15 +1,11 @@
-"use client"
-import { usePathname } from 'next/navigation'
-import React from 'react'
+"use client";
+import { usePathname } from "next/navigation";
+import React from "react";
 
-const page = () => {
-    const pathname = usePathname()
-    const slug = pathname.replace("/", "")
-    return (
-        <div className='max-md:px-8 lg:px-10 px-2' >
-            {slug}
-        </div>
-    )
-}
+const Page = () => {
+  const pathname = usePathname();
+  const slug = pathname.replace("/", "");
+  return <div className="max-md:px-8 lg:px-10 px-2">{slug}</div>;
+};
 
-export default page
+export default Page;
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,51 +1,55 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React, { useState } from "react";
 import { menuNavLinks } from "@/constants";
 const Menu = () => {
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-    return (
-        <div className="w-full bg-[red] lg:px-10 max-lg:hidden relative z-10">
-            <div className="flex justify-between relative">
-                {menuNavLinks.map((item, index) => {
-                    const IconComponent = item.icon; // Dynamically reference the icon
-                    return (
-                        <div
-                            key={index}
-                            className=""
-                            onMouseEnter={() => setHoveredIndex(index)}
-                            onMouseLeave={() => setHoveredIndex(null)}
-                        >
-                            <Link
-                                href={item.route}
-                                className="flex flex-col text-white items-center hover:bg-white hover:text-[#fe0000] rounded-xl px-4 py-2"
-                            >
-                                <IconComponent /> {/* Render the icon component */}
-                                <span>{item.name}</span>
-                            </Link>
-                            {
-                                hoveredIndex === index &&
-                                <div className="absolute left-0 top-full grid grid-cols-4 gap-4 w-full bg-white h-60 rounded-xl border shadow-lg lg:px-10">
-                                    {item.subMenu?.map((sub) => (
-                                        <div className="flex flex-col">
-                                            <h4 className="text-lg font-semibold ">{sub.subMenuTitle}</h4>
-                                            <div className="flex flex-col gap-2">
-                                                {sub.subMenuRoute.map((subroute) => (
-                                                    <span className="text-base hover:text-[#fe0000] cursor-pointer w-fit ml-3">
-                                                        {subroute}
-                                                    </span>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            }
-                        </div>
-                    );
-                })}
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  return (
+    <div className="w-full bg-[red] lg:px-10 max-lg:hidden relative z-10">
+      <div className="flex justify-between relative">
+        {menuNavLinks.map((item, index) => {
+          const IconComponent = item.icon; // Dynamically reference the icon
+          return (
+            <div
+              key={index}
+              className=""
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              <Link
+                href={item.route}
+                className="flex flex-col text-white items-center hover:bg-white hover:text-[#fe0000] rounded-xl px-4 py-2"
+              >
+                <IconComponent /> {/* Render the icon component */}
+                <span>{item.name}</span>
+              </Link>
+              {hoveredIndex === index && (
+                <div className="absolute left-0 top-full grid grid-cols-4 gap-4 w-full bg-white h-60 rounded-xl border shadow-lg lg:px-10">
+                  {item.subMenu?.map((sub) => (
+                    <div key={sub.subMenuTitle} className="flex flex-col">
+                      <h4 className="text-lg font-semibold ">
+                        {sub.subMenuTitle}
+                      </h4>
+                      <div className="flex flex-col gap-2">
+                        {sub.subMenuRoute.map((subroute) => (
+                          <span
+                            key={subroute}
+                            className="text-base hover:text-[#fe0000] cursor-pointer w-fit ml-3"
+                          >
+                            {subroute}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
