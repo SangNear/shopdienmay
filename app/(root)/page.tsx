aea6ab09eddab57eae6c91084734dc12904a7372
Sang Near <nguyenlamsang2004@gmail.com>
2024-09-19 15:28:41 +0700

    update home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Banner from "@/components/custom ui/Banner";
 import ProductCart from "@/components/custom ui/ProductCart";
 import {
@@ -11,15 +12,58 @@ import { Separator } from "@/components/ui/separator";
 import ListProducts from "@/components/custom ui/ListProducts";
 import SystemStock from "@/components/custom ui/SystemStock";
 import TelephoneComponent from "@/components/custom ui/Telephone";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [productSpecials, setProductSpecials] = useState<ProductTypes[]>([])
+
+  const getAllProductSpecials = async () => {
+    try {
+      const res = await fetch("http://api.dienmaygiatotsaigon.vn/api/v1/product/productSpecials", {
+        method: "GET"
+      })
+      const data = await res.json()
+      setProductSpecials(data)
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
+  useEffect(() => {
+    getAllProductSpecials()
+  }, [])
   return (
     <div className="flex flex-col lg:px-20">
       <div className="flex rounded-2xl">
         <Banner />
       </div>
-
-      <div className="flex flex-col justify-between   ">
+      <h2 className="text-2xl font-bold uppercase text-white text-center mt-2 p-2 bg-[#fe0000]">
+        top sản phẩm nổi bật
+      </h2>
+      <div className="  w-full  shadow-2xl  overflow-hidden rounded-2xl py-2">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className=""
+        >
+          <CarouselContent>
+            {productSpecials.map((item, index) => (
+              <CarouselItem
+                key={index}
+                className="max-sm:basis-1/2 max-md:basis-1/3 max-lg:basis-1/4 lg:basis-1/5 flex justify-around"
+              >
+                <ProductCart name={item.name} image={item.images[0]} price={item.price} slug={item.slug} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+      <Separator className="text-[#fe0000] bg-[#fe0000] mt-10" />
+      <div className="flex flex-col justify-between  mt-10 ">
         <div className="w-full h-auto bg-transparent max-md:px-2 mb-3">
           <ListProducts title="tivi" />
         </div>
@@ -31,30 +75,7 @@ export default function Home() {
         </div>
 
 
-        <h2 className="text-2xl font-bold uppercase text-white text-center mt-2 p-2 bg-[#fe0000]">
-          top sản phẩm nổi bật
-        </h2>
-        <div className="  w-full  shadow-2xl  overflow-hidden rounded-2xl py-2">
-          <Carousel
-            opts={{
-              align: "start",
-            }}
-            className=""
-          >
-            <CarouselContent>
-              {Array.from({ length: 9 }).map((_, index) => (
-                <CarouselItem
-                  key={index}
-                  className="max-sm:basis-1/2 max-md:basis-1/3 max-lg:basis-1/4 lg:basis-1/5 flex justify-around"
-                >
-                  <ProductCart />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
+
         <div className=" max-sm:rounded-2xl bg-[#fe0000] max-md mt-2 rounded-2xl mb-4">
           <h2 className="text-2xl uppercase font-bold text-white text-center mt-2 ">
             Ưu đãi dành cho bạn
