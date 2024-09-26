8ae9a3fa9488d3088d5523e5520bde97f1d1ca06
Sang Near <nguyenlamsang2004@gmail.com>
2024-09-26 08:24:28 +0700

    up pri

--- components/custom ui/ProductCart.tsx
+++ components/custom ui/ProductCart.tsx
@@ -12,7 +12,7 @@ const ProductCart = ({ name, slug, image, price }: ProductCartProps) => {
     // Smart-Tivi-Samsung-4K-Crystal-UHD-70-inch-UA70DU7000
     <Link
       href={`/san-pham/${slug ? slug : "Smart-Tivi-Samsung-4K-Crystal-UHD-70-inch-UA70DU7000"}`}
-      className="w-[165px] lg:w-[220px] h-[315px] rounded-xl shadow-xl bg-white flex flex-col justify-between relative">
+      className="w-[165px] lg:w-[220px] h-[320px] rounded-xl shadow-xl bg-white flex flex-col justify-between relative">
       <span className="text-[#fe0000] absolute text-xs top-1 right-1 px-1 rounded-xl border border-[#fe0000] w-fit  ">trả góp 0% </span>
       <div className="p-2 w-full mt-7 ">
         <Image
@@ -26,20 +26,19 @@ const ProductCart = ({ name, slug, image, price }: ProductCartProps) => {
         />
       </div>
       <div className="p-2 w-full">
-        <h4 className="text-sm text-black">
+        <h4 className="text-xs text-black">
 
           {name ? name : "Smart Tivi QLED Cong Samsung 4K 55 Inch 55Q8CNA"}
         </h4>
       </div>
-      <div className="flex-col gap-2 p-2">
+      <div className="flex flex-col  gap-1 p-2 ">
         <span className=" w-full text-[#fe0000]">{price ? formatCurrency(price) : "13,990,000đ"} </span>
-        <div className="flex gap-2 ">
+        <div className="flex  gap-2 ">
           <CheckIcon className="text-xs text-[#78c63b]" />
-          <span className="text-xs text-[#78c63b]">Còn hàng</span>
+          <span className="text-xs text-[#78c63b] text-left">Còn hàng</span>
         </div>
       </div>
     </Link>
-    
   );
 };
 
